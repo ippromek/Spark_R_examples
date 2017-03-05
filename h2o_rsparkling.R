636f4ec40f1fc295b77b638122dfa8dd43fe2cf1
ippromek <ippromek@gmail.com>
2017-03-05 09:26:37 +0000

    Building deep neural nets with h2o and rsparkling that predict arrhythmia of the heart

---
+++ h2o_rsparkling.R
@@ -0,0 +1,3 @@
+# Building deep neural nets with h2o and rsparkling that predict arrhythmia of the heart
+#https://www.r-bloggers.com/building-deep-neural-nets-with-h2o-and-rsparkling-that-predict-arrhythmia-of-the-heart/
+
